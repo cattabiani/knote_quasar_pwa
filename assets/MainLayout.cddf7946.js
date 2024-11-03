a45a1a486a24826aff09ce826dbd9deee7a48114
Katta <alessandro.cattabiani@gmail.com>
2024-11-03 20:56:29 +0100

    Updates

---
+++ assets/MainLayout.cddf7946.js
@@ -0,0 +1,1 @@
+import{_ as a,Q as t,a as n}from"./plugin-vue_export-helper.289b4d30.js";import{r as c,o as s,c as _,w as e,a as o}from"./index.347cc4d0.js";import"./dom.e32953da.js";const i={};function l(u,f){const r=c("router-view");return s(),_(t,{view:"lHr lpR lFr"},{default:e(()=>[o(n,null,{default:e(()=>[o(r)]),_:1})]),_:1})}var v=a(i,[["render",l]]);export{v as default};
